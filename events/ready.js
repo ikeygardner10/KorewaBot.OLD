50a921ffa4d86256e088e3c4e222cde204a4dc78
Ikey <ikeygardner10@gmail.com>
2019-12-09 09:28:56 +1300

    bruh

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,15 @@
 module.exports = client => {
 var servers = client.guilds.size;
-	client.user.setPresence({
-		game: {
-			name: `${client.users.size} users`,
-			type: 'STREAMING',
-			url: 'https://twitter.com/_ikey_'
-		}
-	})
+var users = client.users.size;
+const activities_list = [
+    "$help", 
+    `${servers} servers`,
+	`${users} users`,
+    ];
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+	client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000);
+	
 	console.log(`Logged in as ${client.user.tag}!\n${client.users.size} users, in ${servers} servers connected.`)
 }
